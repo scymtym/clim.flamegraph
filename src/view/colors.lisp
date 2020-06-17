eecbdd9acbfa35c0ea0269d81a3e44264a20b1d4
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-06-17 23:43:01 +0200

    view: Color improvements

--- src/view/colors.lisp
+++ src/view/colors.lisp
@@ -1,44 +1,35 @@
 ;;;; colors.lisp --- Functions for deriving colors from symbols.
 ;;;;
-;;;; Copyright (C) 2017, 2018, 2019 Jan Moringen
+;;;; Copyright (C) 2017, 2018, 2019, 2020 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:clim.flamegraph.view)
 
-(defun hash-color (hash &key (style :light))
+(defun dark? (color)
+  (< (clim:color-ihs color) .5))
+
+(defun hash-color (hash &key (style :light) desaturize?)
   (clim:make-ihs-color (case style
                          (:light 1)
                          (:dark  .5))
                        (/ (mod hash 1024) 1024)
-                       (case style
-                         (:light .3)
-                         (:dark  .8)))
-  #+no (flet ((component (value)
-           (ecase style
-             (:light (+ .3d0 (* .3d0 (/ value 255))))
-             (:dark  (+ .1d0 (* .3d0 (/ value 255)))))))
-
-    (clim:make-rgb-color (component (ldb (byte 8  0) hash))
-                         (component (ldb (byte 8  8) hash))
-                         (component (ldb (byte 8 16) hash)))))
+                       (* (case style
+                            (:light .3)
+                            (:dark  .8))
+                          (if desaturize? .2 1))))
 
 (defun color-variant (base index &key (style :light))
   (let* ((index  (mod index 10))
          (offset (* .2d0 1/9 index)))
     (multiple-value-bind (intensity hue saturation) (clim:color-ihs base)
-      (clim:make-ihs-color (- intensity offset)
-                           hue
-                           saturation)))
-  #+no (let* ((index  (mod index 10))
-              (offset (* .3d0 1/9 index)))
-         (multiple-value-bind (red green blue) (clim:color-rgb base)
-           (clim:make-rgb-color (+ red   offset)
-                                (+ green offset)
-                                (+ blue  offset)))))
+      (clim:make-ihs-color (- intensity offset) hue saturation))))
 
 (defun package-color (package &key (style :light))
-  (hash-color (sxhash (package-name package)) :style style))
+  (hash-color (sxhash (package-name package))
+              :style       style
+              :desaturize? (or (eq package (find-package '#:common-lisp))
+                               (starts-with-subseq "SB-" (package-name package)))))
 
 (defmethod symbol-color ((symbol symbol) &key (style :light))
   (color-variant (if-let ((package (symbol-package symbol)))
@@ -55,13 +46,23 @@
   (let ((base (hash-color 54542 :style style)))
     (color-variant base (sxhash symbol) :style style)))
 
-(defmethod symbol-color ((symbol cons) &key (style :light))
-  (typecase symbol
-    ;; Attempt to get something useful for LAMBDA.
-    ((cons (eql lambda) (cons t (cons symbol #+later (eql in) (cons symbol))))
-     (symbol-color (fourth symbol) :style style))
+(defun symbol-from-list-name (list)
+  ;; Attempt to get something useful for LAMBDA, FLET ... :in ...,
+  ;; etc.
+  (typecase list
+    #+sbcl ((cons (eql lambda) (cons t (cons (eql :in) (cons symbol))))
+            (fourth list))
+    #+sbcl ((cons (member flet labels) (cons t (cons (eql :in) (cons symbol null))))
+            (fourth list))
+    #+sbcl ((cons (member flet labels) (cons symbol t))
+            (second list))
+    #+sbcl ((cons (eql sb-pcl::fast-method) (cons symbol t))
+            (second list))
     (t
-     (symbol-color (or (second symbol) (first symbol)) :style style))))
+     (or (second list) (first list)))))
+
+(defmethod symbol-color ((symbol cons) &key (style :light))
+  (symbol-color (symbol-from-list-name symbol) :style style))
 
 (defmethod symbol-color ((symbol model::qualified-name) &key (style :light))
   (let ((base (hash-color (sxhash (model::container symbol)) :style style)))
